f6fafc96e4c3314c33a19e8d43bd08ff179e5fb8
Marty Thierry <marty@martylouis.com>
2022-07-28 18:30:01 -0500

    feat: add feature grid count
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- src/lib/components/Grid.svelte
+++ src/lib/components/Grid.svelte
@@ -3,8 +3,26 @@
   export let blok: any;
 </script>
 
-<div use:storyblokEditable={blok}>
+<div use:storyblokEditable={blok} class="feature-grid count-{blok.count}">
   {#each blok.columns as blok}
     <StoryblokComponent {blok} />
   {/each}
 </div>
+
+<style lang="postcss">
+  .feature-grid {
+    @apply grid gap-2;
+  }
+
+  .count-2 {
+    @apply grid-cols-2;
+  }
+
+  .count-3 {
+    @apply grid-cols-3;
+  }
+
+  .count-4 {
+    @apply grid-cols-4;
+  }
+</style>
