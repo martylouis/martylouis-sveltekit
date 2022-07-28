bd2ef593e013041fd57608b483366593565ccfb1
Marty Thierry <marty@martylouis.com>
2022-07-28 14:59:15 -0500

    build: add vite https server setup
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+  plugins: [sveltekit(), basicSsl()],
+  server: {
+    port: 3000,
+    https: true
+  }
 };
 
 export default config;
